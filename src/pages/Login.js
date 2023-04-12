5dbd6766e6a32b3367eed7dc8f3e496679039689
GabrielCardiano <gabriel.cardiano1@gmail.com>
2023-04-11 22:21:30 -0300

    req 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
+  handleButton = (event) => {
+    event.preventDefault();
+    const { history, dispatch } = this.props;
+    history.push('/carteira');
+
+    const { email } = this.state;
+    dispatch(userLogin(email));
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const isEmailValid = email.includes('@') && email.toLowerCase().includes('.com');
+    const minNumber = 6;
+    const isPasswordValid = password.length >= minNumber;
+    return (
+      <form onSubmit={ this.handleButton }>
+        <fieldset>
+          <legend>Login</legend>
+          <input
+            type="email"
+            name="email"
+            id="input-email"
+            value={ email }
+            placeholder="Digite seu email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            name="password"
+            id="input-password"
+            value={ password }
+            placeholder="Digite sua senha"
+            minLength="6"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            disabled={ !isEmailValid || !isPasswordValid }
+          >
+            Entrar
+
+          </button>
+        </fieldset>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,14 @@
 // Coloque aqui suas actions
+export const USER_LOGIN = 'USER_LOGIN';
+
+export const userLogin = (email) => ({
+  type: USER_LOGIN,
+  payload: { email },
+});
+
+export const USER_PASSWORD = 'USER_PASSWORD';
+
+export const userPassword = (password) => ({
+  type: USER_PASSWORD,
+  payload: { password },
+});
